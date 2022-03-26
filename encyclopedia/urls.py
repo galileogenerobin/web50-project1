5ab65a02965057faacb75d0f1e55bc749f8a43a8
galileogenerobin <galileogenerobin@gmail.com>
2022-03-26 22:08:21 +0800

    Search
    Updates to meet specification for "Search"
    1. Added a url for search in urls.py
    2. Created a search view in views.py which handles the search functionality
    3. Created 'search.html' that shows the search results in a similar manner to 'index.html'
    4. Updated 'layout.html' so that the search bar sends a GET request to '/search', which will execute views.search as defined in urls.py
    
    Also, updated 'error.html' to no longer mention the desired entry title.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     # Creating a path for an entry_title (e.g. wiki/entry_title) that will execute views.entry, taking the entry_title as a parameter
-    path("wiki/<str:entry_title>", views.entry, name="entry")
+    path("wiki/<str:entry_title>", views.entry, name="entry"),
+    # Creating a path for search
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from . import util
 
@@ -22,5 +24,26 @@ def entry(request, entry_title):
         })
     else:
         # Render the apology page
-        return render(request, "encyclopedia/error.html", {"entry_title": entry_title})
+        return render(request, "encyclopedia/error.html")
 
+
+# Handle search results
+def search(request):
+    # Get the search query
+    search_query = request.GET['q']
+
+    # Check for entries that have the search query as a substring (or could be the exact match)
+    # We need to convert the serach query and the entry to lower case so that our search is not case sensitive
+    search_results = [entry for entry in util.list_entries() if search_query.lower() in entry.lower()]
+
+    # Check if there is an exact match, not case sensitive
+    for entry in search_results:
+        if search_query.lower() == entry.lower():
+            # Redirect to the results page for that entry
+            return HttpResponseRedirect("wiki/{}".format(entry))
+    
+    # Otherwise, we render the search results in the index page
+    return render(request, "encyclopedia/search.html", {
+        "search_query": search_query,
+        "search_results": search_results
+    })
